1b309ff3c42fa8b2670031387b3ebb70103eea06
lfisher447-afk <lfisher447@gmail.com>
2026-04-09 00:02:40 -0500

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,11 @@
 module.exports = {
-  apps: [
-    {
-      name: 'Teneflix-Engine',
-      script: './api/index.js',
-      instances: 'max', // Utilizes all available CPU cores
-      exec_mode: 'cluster',
-      autorestart: true,
-      max_memory_restart: '500M',
-      log_date_format: 'YYYY-MM-DD HH:mm Z',
-      env: {
-        NODE_ENV: 'production',
-      },
-    },
-  ],
+  apps: [{
+    name: 'Teneflix',
+    script: './api/index.js',
+    instances: 'max',
+    exec_mode: 'cluster',
+    autorestart: true,
+    max_memory_restart: '1G',
+    env: { NODE_ENV: 'production' }
+  }]
 };
